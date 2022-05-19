f308fb1f6270be0df55b64bd51ecb6be8d161c85
Filipe Prado - Rethink <filipe.prado@rethink.dev>
2022-05-19 12:30:09 -0300

    Adicionando Firestore ao app

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,8 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 import { firebaseConfig } from "./config";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Firestore
 
-console.log({ app });
+const db = getFirestore(app);
+
+const firebaseInstance = { app, db };
+
+export default firebaseInstance;
