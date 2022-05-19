c64802daabdbee1cb89e7fb8c76b50d260ce2c79
Filipe Prado - Rethink <filipe.prado@rethink.dev>
2022-05-19 17:28:45 -0300

    Configurando sistema de rotas com react-router-dom

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Vamos criar um sistema que deve permitir que o usuário crie snippets de código
 ✅ Iniciar boilerplate com React e fazer ajustes.\
 ✅ Configurar Firebase e Firestore.\
 ✅ Configurar deploy na vercel.\
-⬜️ Implementar sistema de rotas usando `react-router-dom.`\
+✅ Implementar sistema de rotas usando `react-router-dom.`\
 ⬜️ Implementar sistema de Autenticação usando `conxtex api`.
 
 <br>
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Images from "./assets";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Screens
+import LoginScreen from "./screens/login/LoginScreen";
+import DashboardScreen from "./screens/dashboard/DashboardScreen";
 
 function App() {
   return (
-    <div>
-      <img src={Images.logo.default} alt='logo padrão Nothink' />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<LoginScreen />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/dashboard' element={<DashboardScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
