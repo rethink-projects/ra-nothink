938fe50d7df9a007a288b80def4b4c9ee3b2f080
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2022-05-26 11:30:48 -0300

    Inicializando boilerplate em react e ajustando estrutura css

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+import logo from "./logo.svg"
+import githubIcon from "./logo.svg"
+
+
+const Images = {
+    logo: {
+        default: logo
+    },
+    icons: {
+        github: githubIcon
+    }
+}
+
+export default Images;
