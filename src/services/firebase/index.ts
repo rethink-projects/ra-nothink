14170c9b5a5ee11908294d123f9acd0f51f14fc4
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-05-27 10:24:52 -0300

    Firebase

---
+++ src/services/firebase/index.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import { firebaseConfig } from "./config";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const firebaseInstance = { app, db };
+
+export default firebaseInstance;
