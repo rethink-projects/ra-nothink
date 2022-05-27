4c83b6582ee6e2a9e7716e5677cf3cb841245c4e
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-05-27 11:56:09 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Imagens from "./assets";
+import Dashboard from "./screens/dashboard/Dashboard";
+import LoginScreen from "./screens/login/LoginScreen";
 
 function App() {
   return (
-    <div>
-      <img src={Imagens.logo.default} alt="Logo Padrão Rethink" />
-      <h3>Anotações rápidas para devs!</h3>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LoginScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
