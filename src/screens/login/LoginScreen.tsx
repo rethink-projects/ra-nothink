17181fd0678ad1b812a47a821376675e5a197b9b
gabsrethink <gabriel.melo@rethink.dev>
2022-05-31 12:13:08 -0300

    Arrumando a arrow da tela de login e tirando mensagens de warning

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,22 +1,13 @@
-import { useAuth } from "../../context/AuthContext";
 import Form from "./components/Form/Form";
 import Presentation from "./components/Presentation/Presentation";
 import styles from "./LoginScreen.module.css";
 
 export const LoginScreen = () => {
-  let auth = useAuth();
-
-  if (!auth.user) {
-    return (
-      <div className={styles.container}>
-        <Presentation />
-        <Form />
-      </div>
-    );
-  }
+ 
   return (
-    <div>
-      <p>{auth.user?.name}</p>
+    <div className={styles.container}>
+      <Presentation />
+      <Form />
     </div>
   );
 };
