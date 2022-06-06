6ac60c407ef9bd8df829b28b6881f8b3741cdd3d
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-06-06 14:56:41 -0300

    Configurando e inicializando Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "./services/firebase";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
