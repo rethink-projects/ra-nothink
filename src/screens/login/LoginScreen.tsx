2e05b5130f2dd02a8958124b68e7fda8a6a77a96
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-06-06 11:02:40 -0300

    Adicionando sistema de autenticacao de rotas

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 export default function LoginScreen() {
+  let auth = useAuth();
+  let navigate = useNavigate();
+
+  const handleLogin = () => {
+    auth.signin(
+      {
+        name: "Felipe Reggiane",
+        avatarUrl: "https://www.github.com/FelipeReggiane.png",
+        email: "felipe.reggiane@rethink.dev",
+      },
+      () => navigate("/dashboard", { replace: true })
+    );
+  };
+
+  useEffect(() => {
+    let localStorageUser = localStorage.getItem("@nothink:user");
+    console.log({ localStorageUser });
+    if (localStorageUser) {
+      auth.signin(JSON.parse(localStorageUser), () =>
+        navigate("/dashboard", { replace: true })
+      );
+    }
+  }, [auth, navigate]);
+
+  if (!auth.user) {
+    return (
+      <div>
+        <h1>LoginScreen</h1>
+        <button onClick={handleLogin}>Fazer Login</button>
+      </div>
+    );
+  }
   return (
-    <div>LoginScreen</div>
-  )
+    <div>
+      <h1>LoginScreen</h1>
+      <p>{auth.user?.name}</p>
+    </div>
+  );
 }
---
+++ src/services/auth/Auth.tsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+type RequireAuthType = {
+  children: JSX.Element;
+};
+
+function RequireAuth({ children }: RequireAuthType) {
+    let auth = useAuth();
+    let location = useLocation();
+
+    if(!auth.user){
+        return <Navigate to="/login" state={{from: location}} replace />
+    }
+
+    return children;
+}
+
+export default RequireAuth;
