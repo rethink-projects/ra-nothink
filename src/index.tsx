2a3a6dda817a78f2c69b56a4f1ffe62c2f768743
fabianakamo <fabiana.kamo@rethink.dev>
2022-06-08 17:34:35 -0300

    Configurando e inicializando Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 // CSS
-import "./theme/global.css"
+import "./theme/global.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./services/firebase";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
