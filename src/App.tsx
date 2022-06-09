05d8c275fcf5a66b79ee032084b6d3fb1ba82b1c
carolinavaleriano <102779256+carolinavaleriano@users.noreply.github.com>
2022-06-09 10:02:20 -0300

    Configurando sistema de rotas com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import Images from "./assets";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Screens
+import DashboardScreen from "./screens/dashboard/DashboardScreen";
+import LoginScreen from "./screens/login/LoginScreen";
 
 function App() {
   return (
-    <div>
-      <img src={Images.logo.default} alt="logo padrão nothink" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+            <Route index element={<LoginScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/dashboard" element={<DashboardScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
