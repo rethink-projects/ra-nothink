bf91dad81904e174deb3eed7e689ed09e29b014d
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-06-10 16:23:48 -0300

    Componentizando botões de login

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export {default as IconButton} from "./ui/iconButton/IconButton";
+export {default as Divider} from "./ui/divider/Divider";
---
+++ src/components/ui/divider/Divider.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./Divider.module.css";
+
+const Divider = () => {
+  return (
+    <div className={styles.divider_container}>
+      <div className={styles.divider} />
+      <p className={styles.divider_text}>ou</p>
+      <div className={styles.divider} />
+    </div>
+  );
+};
+
+export default Divider;
