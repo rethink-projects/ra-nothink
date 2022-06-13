1314780d6bd6374dc1a87ce243099a2457dc0bbe
carolinavaleriano <102779256+carolinavaleriano@users.noreply.github.com>
2022-06-13 14:15:19 -0300

    Adicionando responsividade para a tela de login

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as IconButton } from "./ui/IconButton/IconButton";
+
+export { default as Divider } from "./ui/Divider/Divider";
---
+++ src/components/ui/IconButton/IconButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Images from "../../../assets";
+import styles from "./IconButton.module.css";
+
+type IconButtonProps = {
+  type: "google" | "github";
+  onClick?: () => void;
+};
+
+function IconButton({ type, onClick }: IconButtonProps) {
+  const defaultIconClass =
+    type == "github"
+      ? styles.defaultIconButton
+      : `${styles.defaultIconButton} ${styles.darkButtonClass}`;
+  return (
+    <button onClick={onClick} className={defaultIconClass}>
+      <img
+        className={styles.default_img}
+        src={Images.icons[type]}
+        alt={type == "google" ? "Google Icon Nothink" : "Github Icon Nothink"}
+      />
+      Entrar com o {type == "google" ? "Google" : "Github"}
+    </button>
+  );
+}
+
+export default IconButton;
